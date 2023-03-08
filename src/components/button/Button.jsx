b7426816dcffc546b7177ead94cca932e9c0bdfc
dariomrk <dariomrkonjic1@gmail.com>
2023-03-08 20:21:59 +0100

    Implement Button component.

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Button.scoped.css';
 
-function Button({ text }) {
+/**
+ * @param {{
+ * text: string,
+ * type: undefined | 'danger' | 'success',
+ * callback: Function}} props
+ * @returns {JSX.Element} Button component
+ */
+function Button({ text, type, callback }) {
   return (
-    <button>
-      {props.text}
+    <button className={`${type}`} type="button" onClick={(e) => callback(e)}>
+      {text}
     </button>
   );
 }

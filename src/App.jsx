e1f2839007f246c4d6a56b2ec7edf66bdc482b4e
dariomrk <dariomrkonjic1@gmail.com>
2023-03-08 23:45:07 +0100

    Add temp mock data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import CardContainer from './components/cardContainer/CardContainer';
+import { Album } from './lib/album';
+
+// Temp mock data
+const albums = [
+  new Album({
+    name: 'Rage Against the Machine',
+    author: 'Rage Against the Machine',
+    genres: ['Rap metal', 'Alternative metal', 'Funk metal'],
+    yearOfRelease: 1992,
+  }),
+  new Album({
+    name: 'Californication',
+    author: 'Red Hot Chili Peppers',
+    genres: ['Alternative rock', 'Funk rock'],
+    yearOfRelease: 1998,
+  }),
+  new Album({
+    name: 'Ride the Lightning',
+    author: 'Metallica',
+    genres: ['Thrash metal'],
+    yearOfRelease: 1984,
+  }),
+  new Album({
+    name: 'Dirt',
+    author: 'Alice In Chains',
+    genres: ['Grunge', 'Alternative rock'],
+    yearOfRelease: 1992,
+  }),
+];
 
 function App() {
   return (
-    <>
-    </>
+    <CardContainer albums={albums} />
   );
 }
 

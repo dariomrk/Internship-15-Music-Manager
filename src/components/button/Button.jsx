473d800b74c0f39542f319b3d031ae306b783593
dariomrk <dariomrkonjic1@gmail.com>
2023-03-08 17:10:46 +0100

    Add Button component.

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.scoped.css';
+
+function Button({ text }) {
+  return (
+    <button>
+      {props.text}
+    </button>
+  );
+}
+
+export default Button;
